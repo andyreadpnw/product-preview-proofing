7a24640e0750907b492db6259b9cae7e37427a2a
Andy Read <andyreadpnw@gmail.com>
2020-02-06 13:29:10 -0800

    Updates

---
+++ precache-manifest.b971d3116a4257797cba4da6f40685e2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55db6a7e6c1bf0c43edbf097ff12c3e9",
+    "url": "/product-preview-proofing/index.html"
+  },
+  {
+    "revision": "571d533c082badf61bb3",
+    "url": "/product-preview-proofing/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "558cd4aae09b12bad9b3",
+    "url": "/product-preview-proofing/static/js/2.baea11f6.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/product-preview-proofing/static/js/2.baea11f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "571d533c082badf61bb3",
+    "url": "/product-preview-proofing/static/js/main.a0276d3a.chunk.js"
+  },
+  {
+    "revision": "5c155f2e46d09276496b",
+    "url": "/product-preview-proofing/static/js/runtime-main.8d608c1c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/product-preview-proofing/static/media/logo.5d5d9eef.svg"
+  }
+]);
