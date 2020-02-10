1922e75fed5decea875abeacb8f447a7b93d717b
Andy Read <andyreadpnw@gmail.com>
2020-02-10 12:26:44 -0800

    Updates

--- precache-manifest.234eeef2e1bb056aea7b65b61df8690b.js
+++ precache-manifest.234eeef2e1bb056aea7b65b61df8690b.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "739500ff1e800a02f88097af2e42b4b4",
+    "revision": "56e4322138458fba2ed7cf0bc0c178fd",
     "url": "/product-preview-proofing/index.html"
   },
   {
-    "revision": "8513b697cdc5ba010210",
+    "revision": "86068156ea00d80305a3",
     "url": "/product-preview-proofing/static/css/2.25d2dfdd.chunk.css"
   },
   {
-    "revision": "1b0476e660bbb3051416",
+    "revision": "5587fb2a626ff2ed9366",
     "url": "/product-preview-proofing/static/css/main.cb9b0e0c.chunk.css"
   },
   {
-    "revision": "8513b697cdc5ba010210",
-    "url": "/product-preview-proofing/static/js/2.a3e58a9c.chunk.js"
+    "revision": "86068156ea00d80305a3",
+    "url": "/product-preview-proofing/static/js/2.bd4016fc.chunk.js"
   },
   {
     "revision": "9035b4332ca6c07de8646b37cea92066",
-    "url": "/product-preview-proofing/static/js/2.a3e58a9c.chunk.js.LICENSE.txt"
+    "url": "/product-preview-proofing/static/js/2.bd4016fc.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "1b0476e660bbb3051416",
-    "url": "/product-preview-proofing/static/js/main.ef19099c.chunk.js"
+    "revision": "5587fb2a626ff2ed9366",
+    "url": "/product-preview-proofing/static/js/main.3dcefd35.chunk.js"
   },
   {
     "revision": "5c155f2e46d09276496b",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/product-preview-proofing/precache-manifest.a0870bed30f57fc9f678a07ce1660c13.js"
+  "/product-preview-proofing/precache-manifest.234eeef2e1bb056aea7b65b61df8690b.js"
 );
 
 self.addEventListener('message', (event) => {
